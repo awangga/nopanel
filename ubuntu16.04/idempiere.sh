e92d9a82d12ce7ca256ba4d1e9f9596d39a1489b
Rolly Maulana Awangga <rolly@awang.ga>
2017-06-21 13:42:47 +0700

    ok

--- ubuntu16.04/idempiere.sh
+++ ubuntu16.04/idempiere.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-sudo apt-get install postgresql
-sudo apt-get install jdk8
+
 
 wget https://sourceforge.net/projects/idempiere/files/v3.1/daily-server/idempiereServer3.1Daily.gtk.linux.x86_64.zip/download
 mv download idempiereServer3.1Daily.gtk.linux.x86_64.zip
@@ -8,8 +7,14 @@ unzip idempiereServer3.1Daily.gtk.linux.x86_64.zip
 wget https://sourceforge.net/projects/idempiere/files/v3.1/daily-server/idempiereServer3.1Daily.gtk.linux.x86_64.deb/download
 mv download idempiereServer3.1Daily.gtk.linux.x86_64.deb
 
-sudo apt-get update
-sudo apt-get install default-jdk
-sudo apt-get install postgresql postgresql-contrib
+apt-get update
+apt-get install default-jdk
+apt-get install postgresql postgresql-contrib
+echo "host		all		all		192.168.100.0/24	md5" >> /etc/postgresql/9.5/main/pg_hba.conf
+systemctl restart postgresql
+echo "set password for user postgres"
+passwd postgres
+echo "go to psql and run : ALTER USER postgres PASSWORD 'newpasword';\q; GRANT ALL PRIVILEGES ON database idempiere TO adempiere;"
+su postgres
 
-https://www.digitalocean.com/community/tutorials/how-to-install-and-use-postgresql-on-ubuntu-16-04
+sh console-setup.sh
