c3c5245cdf002416a0d26b4546f179d0db56330e
Rolly Maulana Awangga <rolly@awang.ga>
2017-07-18 12:18:17 +0700

    update gitlab

--- ubuntu16.04/gitlab.sh
+++ ubuntu16.04/gitlab.sh
@@ -6,3 +6,6 @@ apt-get install gitlab-ce
 gitlab-ctl reconfigure
 
 
+https://github.com/awangga/gitlab
+https://www.digitalocean.com/community/tutorials/how-to-secure-gitlab-with-let-s-encrypt-on-ubuntu-16-04
+
