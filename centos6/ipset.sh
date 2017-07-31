afa81a81f7696e8aa8e8696e6db46a300f66d5a7
Rolly Maulana Awangga <rolly@awang.ga>
2017-07-31 13:42:00 +0700

    fix bug

--- centos6/ipset.sh
+++ centos6/ipset.sh
@@ -5,5 +5,5 @@ sed -i 's/NM_CONTROLLED=yes/NM_CONTROLLED=no/g' /etc/sysconfig/network-scripts/i
 sed -i 's/BOOTPROTO=dhcp/BOOTPROTO=static/g' /etc/sysconfig/network-scripts/ifcfg-$1
 echo "IPADDR=$2" >> /etc/sysconfig/network-scripts/ifcfg-$1
 echo "NETMASK=$3" >> /etc/sysconfig/network-scripts/ifcfg-$1
-echo "GATEWAY=$3" >> /etc/sysconfig/network-scripts/ifcfg-$1
-echo "DNS1=$4" >> /etc/sysconfig/network-scripts/ifcfg-$1
+echo "GATEWAY=$4" >> /etc/sysconfig/network-scripts/ifcfg-$1
+echo "DNS1=$5" >> /etc/sysconfig/network-scripts/ifcfg-$1
--- proxmox/createcluster.sh
+++ proxmox/createcluster.sh
@@ -3,3 +3,10 @@ pvecm create belant
 
 #on client
 pvecm add 172.16.1.1
+
+
+add harddisk
+fdisk -l
+cat /proc/partitions
+lsblk
+
