8af796833faa2056eabfee9c930fc04c30a16ab9
Rolly Maulana Awangga <rolly@awang.ga>
2020-04-18 10:31:25 +0700

    add mooodle setting

--- ubuntu18.04/moodle.sh
+++ ubuntu18.04/moodle.sh
@@ -1,10 +1,10 @@
 sudo apt install php7.1-zip php7.1-intl php7.1-xmlrpc php7.1-soap
 mariadb -u root -p
 
-#CREATE DATABASE mila DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-#GRANT ALL ON mila.* TO 'mila'@'localhost' IDENTIFIED BY 'rollyganteng';
-MariaDB [(none)]> FLUSH PRIVILEGES;
-MariaDB [(none)]> EXIT;
+#MariaDB [(none)]> CREATE DATABASE mila DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+#MariaDB [(none)]> GRANT ALL ON mila.* TO 'mila'@'localhost' IDENTIFIED BY 'rollyganteng';
+#MariaDB [(none)]> FLUSH PRIVILEGES;
+#MariaDB [(none)]> EXIT;
 
 cd /tmp
 wget https://download.moodle.org/stable38/moodle-latest-38.tgz
@@ -18,7 +18,7 @@ chown -R www-data:www-data miladata/
 chmod -R 0770 miladata/
 sudo nano /etc/nginx/sites-enabled/mila
 
-
+#########################################
 server {
     server_name mila.stimlog.ac.id;
     root /var/www/html/mila;
@@ -41,12 +41,10 @@ server {
     }
 
 }
+############################################
 
-
-
-
-config.php
-
+sudo nano /var/www/html/mila/config.php
+#############################################
 <?php  // Moodle configuration file
 
 unset($CFG);
@@ -64,7 +62,7 @@ $CFG->dboptions = array (
   'dbpersist' => 0,
   'dbport' => '',
   'dbsocket' => '',
-  'dbcollation' => 'utf8_unicode_ci',
+  'dbcollation' => 'utf8mb4_general_ci',
 );
 
 $CFG->wwwroot   = 'http://mila.stimlog.ac.id';
@@ -77,3 +75,4 @@ require_once(__DIR__ . '/lib/setup.php');
 
 // There is no php closing tag in this file,
 // it is intentional because it prevents trailing whitespace problems!
+###############################################
--- ubuntu18.04/nginxphpmysql.sh
+++ ubuntu18.04/nginxphpmysql.sh
@@ -11,7 +11,7 @@ sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update	
 
 #php 5.6
-sudo apt install php5.6-fpm php5.6-mbstring php5.6-xml php5.6-mysql php5.6-common php5.6-gd php5.6-json php5.6-cli php5.6-curl
+sudo apt install php5.6-fpm php5.6-mbstring php5.6-xml php5.6-mysql php5.6-common php5.6-gd php5.6-json php5.6-cli php5.6-curl php5.6-zip php5.6-intl php5.6-xmlrpc php5.6-soap
 sudo systemctl start php5.6-fpm
 sudo systemctl status php5.6-fpm
 sudo systemctl enable php5.6-fpm
@@ -23,7 +23,7 @@ sudo systemctl status php7.1-fpm
 sudo systemctl enable php7.1-fpm
 
 #pphp 7.4
-sudo apt install php7.4-fpm php7.4-mbstring php7.4-xml php7.4-mysql php7.4-common php7.4-gd php7.4-json php7.4-cli php7.4-curl
+sudo apt install php7.4-fpm php7.4-mbstring php7.4-xml php7.4-mysql php7.4-common php7.4-gd php7.4-json php7.4-cli php7.4-curl php7.4-zip php7.4-intl php7.4-xmlrpc php7.4-soap
 sudo systemctl start php7.4-fpm
 sudo systemctl status php7.4-fpm
 sudo systemctl enable php7.4-fpm
