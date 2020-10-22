de8c3a0b821b77ba1dfa4b39538cf752415eafa6
Rolly Maulana Awangga <rolly@awang.ga>
2020-10-22 14:07:27 +0700

    updates

--- docker/php/Dockerfile.php
+++ docker/php/Dockerfile.php
@@ -17,5 +17,20 @@ RUN apt-get clean && rm -rf /var/lib/apt/lists/*
 RUN docker-php-ext-install pgsql pdo_pgsql pdo_mysql exif pcntl bcmath gd intl xmlrpc soap
 
 # Get latest Composer
-# COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
+COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
 
+# Create system user to run Composer and Artisan Commands
+RUN useradd -G www-data,root -u $uid -d /home/$user $user
+RUN mkdir -p /home/$user/.composer && \
+    chown -R $user:$user /home/$user
+
+#RUN chown -R www-data:www-data /var/www
+#COPY --chown=$user:$user . /var/www/html
+	
+# Set working directory
+WORKDIR /var/www
+
+USER $user
+# Expose port 9000 and start php-fpm server
+EXPOSE 9000
+CMD ["php-fpm"]
