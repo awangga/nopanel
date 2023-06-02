cdfb3d58b2ca9440ee2e8271d95ef6eeca167d78
Rolly Maulana Awangga <rolly@awang.ga>
2023-06-02 07:40:31 +0700

    gcp

---
+++ gcp.sh
@@ -0,0 +1,5 @@
+sudo dpkg-reconfigure tzdata
+sudo add-apt-repository ppa:longsleep/golang-backports
+sudo apt update
+sudo apt -y install golang-go tmux unzip python3 python3-pip
+sudo pip3 install gdown
